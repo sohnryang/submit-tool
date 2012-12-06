649cf3f51cad3cc93a8c206e654a26a3e271936b
Baekjoon Choi <greenfreakzilla@gmail.com>
2012-12-07 03:09:47 +0900

    결과도 볼 수 있음

--- submit.py
+++ submit.py
@@ -4,12 +4,13 @@ import urllib, urllib2
 import sys
 import json
 import os
+import time
 from getpass import getpass
 r=lambda : sys.stdin.readline()
 
 def get_source(filename):
     if not os.path.exists(filename):
-        return ({'error':True,'error_text':'%s는 없는 파일 입니다.'%filename},'')
+        return ({'error':True,'error_text':'%s는 없는 파일 입니다'%filename},'')
     fp = open(filename,'r')
     source = fp.read()
     fp.close()
@@ -86,6 +87,31 @@ def submit(username, password, problem_id, source, language):
     result = json.loads(result)
     return result
 
+def get_result(solution_id, key):
+    results = ['기다리는 중','재채점을 기다리는 중',
+    '컴파일 하는 중','채점중','맞았습니다!!',
+    '출력 형식이 잘못되었습니다','틀렸습니다','시간 초과',
+    '메모리 초과','출력 초과','런타임 에러','컴파일 에러']
+    print '채점 번호: %d' % solution_id
+    while True:
+        url = 'http://www.acmicpc.net/cmd/status.php'
+        values = {'solution_id':solution_id, 'key':key}
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url,data)
+        response = urllib2.urlopen(req)
+        result = response.read()
+        response.close()
+        result = json.loads(result)
+        if result['error']:
+            print result['error_text']
+            return
+        result = result['result']
+        print results[int(result['result'])]
+        if int(result['result']) >= 4:
+            print u'메모리: %(memory)s KB\n시간: %(time)s MS\n코드 길이: %(code_length)s B' % result
+            break
+        time.sleep(1)
+
 def main():
     argv = sys.argv[1:]
     if len(argv) != 2:
@@ -100,21 +126,21 @@ def main():
         return
     language = get_language(filename)
     if language == -1:
-        print '무슨 언어인지 모르겟어요. 확장자를 확인해 주세요.'
+        print '무슨 언어인지 모르겟어요. 확장자를 확인해 주세요'
         return
     sys.stdout.write('아이디: ')
     username = r().strip()
     password = getpass('비밀번호: ')
     if len(username) == 0:
-        print '아이디를 입력해 주세요.'
+        print '아이디를 입력해 주세요'
         return
     if len(password) == 0:
-        print '비밀번호를 입력해 주세요.'
+        print '비밀번호를 입력해 주세요'
         return
     res = submit(username,password,problem_id,source,language)
     if res['error']:
         print res['error_text']
         return
-    
+    get_result(res['solution_id'], res['key'])
 if __name__ == '__main__':
     main()
