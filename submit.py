58da47638db7572c35d3491f0e286fbef2b4d218
Baekjoon Choi <greenfreakzilla@gmail.com>
2012-12-07 02:02:37 +0900

    언어 추가

--- submit.py
+++ submit.py
@@ -14,6 +14,64 @@ def get_source(filename):
     fp.close()
     return ({'error':False},source)
 
+def get_language(filename):
+    dummy, extension = os.path.splitext(filename)
+    if len(extension) == 0:
+        return -1
+    if extension in ['.c']:
+        return 0
+    elif extension in ['.cpp','.cc','cxx']:
+        return 1
+    elif extension in ['.pas']:
+        return 2
+    elif extension in ['.java']:
+        return 3
+    elif extension in ['.rb']:
+        return 4
+    elif extension in ['.sh']:
+        return 5
+    elif extension in ['.py']:
+        return 6
+    elif extension in ['.php']:
+        return 7
+    elif extension in ['.pl']:
+        return 8
+    elif extension in ['.cs']:
+        return 9
+    elif extension in ['.m']:
+        return 10
+    elif extension in ['.go']:
+        return 12
+    elif extension in ['.f95','.for']:
+        return 13
+    elif extension in ['.scm']:
+        return 14
+    elif extension in ['.scala']:
+        return 15
+    elif extension in ['.lua']:
+        return 16
+    elif extension in ['.js']:
+        return 17
+    elif extension in ['.coffee']:
+        return 18
+    elif extension in ['.ada']:
+        return 19
+    elif extension in ['.vb']:
+        return 20
+    elif extension in ['.awk']:
+        return 21
+    elif extension in ['.ml']:
+        return 22
+    elif extension in ['.bf']:
+        return 23
+    elif extension in ['.ws']:
+        return 24
+    elif extension in ['.tcl']:
+        return 26
+    elif extension in ['.d']:
+        return 29
+    return -1
+
 def submit(username, password, problem_id, source, language):
     url = 'http://www.acmicpc.net/cmd/submit.php'
     values = {'username':username, 'password':password,
@@ -39,7 +97,10 @@ def main():
     if res['error']:
         print res['error_text']
         return
-
+    language = get_language(filename)
+    if language == -1:
+        print 'cannot determine language'
+        return
     sys.stdout.write('Username: ')
     username = r().strip()
     password = getpass()
