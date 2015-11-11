78f1b101990ff27b75f4ed1235cc671fe05bcc87
hyunsukoh <algoshipda@gmail.com>
2015-11-11 22:00:30 +0900

    비밀번호 받을때 에러 나서 고쳤음

--- submit.py
+++ submit.py
@@ -24,7 +24,7 @@ def get_language(filename):
     if extension in ['.c']:
         return 0
     elif extension in ['.cpp','.cc','.cxx']:
-        return 1
+        return 49
     elif extension in ['.p','.pas']:
         return 2
     elif extension in ['.java']:
@@ -194,7 +194,8 @@ def main():
         return
     sys.stdout.write(u'아이디: ')
     username = r().strip()
-    password = getpass(u'비밀번호: ')
+    sys.stdout.write(u'비밀번호: ')
+    password = getpass('')
     if len(username) == 0:
         print u'아이디를 입력해 주세요'
         return
